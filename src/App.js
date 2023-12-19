761da97f232e6eec7d89a479939405c03d77682c
Mucahit <mucahit.kartal2015@gmail.com>
2023-12-19 23:24:27 +0300

    Final

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import "./App.css";
-import Navbar from "./components/navbar/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
 import Projects from "./pages/projects/Projects";
 import Contact from "./pages/contact/Contact";
-import Footer from "./components/footer/Footer";
+import Footer from "./components/Footer/Footer";
 import "./scss/main.scss";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import NotFound from "./components/Error/NotFound";
 
 function App() {
+  const location = useLocation();
+
+  const getPagesStyles = () => {
+    if (location.pathname === "/") {
+      return "home-bgImg-container";
+    } else if (location.pathname === "/about") {
+      return "about-bgImg-container";
+    } else if (location.pathname === "/projects") {
+      return "project-bgImg-container";
+    } else if (location.pathname === "/contact") {
+      return "contact-bgImg-container";
+    }
+  };
+
   return (
-    <div>
+    <div className={getPagesStyles()}>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
